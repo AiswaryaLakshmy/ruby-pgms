56e899f422d06f2845804d3e67959e0df1f66246
AiswaryaLakshmy <aiswarya@7nodes.com>
2016-06-30 16:14:36 +0530

    unless, while, begin whiledone

--- task.rb
+++ task.rb
@@ -3,18 +3,53 @@
 class Conditionstatements
 
 	def first
+		puts '...........First..........'
 		x = 1
 		if x > 2
-   			puts "x is greater than 2"
-		elsif x <= 2 and x!=0
-   			puts "x is 1"
+   			puts 'x is greater than 2'
+		elsif x <= 2 and x != 0
+   			puts 'x is 1'
 		else
    			puts "I can't guess the number"
 		end
 	end
 
+	def second
+		puts '.............Second..........'
+		x = 1
+		unless x > 2
+		   puts 'x is less than 2'
+		 else
+		  puts 'x is greater than 2'
+		end
+	end
+
+	def third
+		puts '............Third...........'
+		i = 0
+		num = 5
+
+		while i < num  do
+		   puts "Inside the loop i = #{i}"
+		   i += 1
+		end
+	end
+
+	def fourth
+		puts '...........Fourth............'
+		i = 1
+		num = 8
+		begin
+		   puts "Inside loop i = #{i}"
+		   i += 1
+		end while i < num
+	end
+
 	obj = Conditionstatements.new
 	obj.first
+	obj.second
+	obj.third
+	obj.fourth
 end
 
 			 	 
