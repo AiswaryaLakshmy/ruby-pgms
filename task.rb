30468a830e23c1298df9a50d1611815961b3be22
AiswaryaLakshmy <aiswarya@7nodes.com>
2016-06-30 15:48:53 +0530

    if elsif loop

---
+++ task.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby -w
+
+class Conditionstatements
+
+	def first
+		x = 1
+		if x > 2
+   			puts "x is greater than 2"
+		elsif x <= 2 and x!=0
+   			puts "x is 1"
+		else
+   			puts "I can't guess the number"
+		end
+	end
+
+	obj = Conditionstatements.new
+	obj.first
+end
+
+			 	 
